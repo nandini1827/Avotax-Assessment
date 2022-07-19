89ed172f0bc0118c2cccab066a6dd65df864938e
Nandini <ngoyal_be20@thapar.edu>
2022-07-19 20:15:17 +0530

    assessment 2

---
+++ 2.js
@@ -0,0 +1,13 @@
+function minimumWaitingTime(queries) {
+    queries.sort((a, b) => a - b);
+  
+    let nextWaitingTime = 0;
+    let totalWaitingTime = 0;
+  
+    for (const duration of queries) {
+      totalWaitingTime += nextWaitingTime;
+      nextWaitingTime += duration;
+    }
+  
+    return totalWaitingTime;
+  }
